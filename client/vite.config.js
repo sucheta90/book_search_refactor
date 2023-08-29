67c1c0ec4d4b7e287a369ad27c4bde4ec607d0cc
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2023-08-29 08:17:36 -0400

    copied over directories from develop/class repo

---
+++ client/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+    // We are pointing our Vite client-side development server to proxy API requests to our server-side Node server at port 3001.
+    // Without this line, API calls would attempt to fallback and query for data from the current domain: localhost:3000
+    proxy: {
+      "/graphql": {
+        target: "http://localhost:3001",
+        secure: false,
+        changeOrigin: true,
+      },
+    },
+  },
+});
